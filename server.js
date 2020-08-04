2903afe0a77d97f4aa4206ab23937ee0f7902b31
paperlambda <revzankamarullah@gmail.com>
2020-08-04 22:21:10 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const path = require('path')
+const graceful = require('node-graceful')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const app = express()
+const PORT = process.env.PORT || 8080
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'dist')))
+
+mongoose.connect('mongodb://127.0.0.1:27017/hive', {
+    useNewUrlParser: true
+})
+
+const connection = mongoose.connection
+connection.once('open', function () {
+    console.log('MongoDB connection established')
+})
+
+app.get('*', (req, res) => {
+    return res.sendFile(path.join(`${__dirname}/dist/index.html`))
+})
+
+const server = app.listen(PORT, err => {
+    if (err) {
+        console.error(err)
+    }
+
+    console.log(`Running on port ${PORT}`)
+})
+
+graceful.on('exit', done => {
+    server.close(() => {
+        done()
+    })
+})
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'core-js/stable'
+import 'regenerator-runtime'
+import React from 'react'
+import { render } from 'react-dom'
+// import App from './App'
+
+render(<h1>Successful Mount</h1>, document.getElementById('app'))
